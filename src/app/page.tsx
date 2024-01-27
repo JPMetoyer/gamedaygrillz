273a59b48fb313f555591b11f56c28e152f64fcc
JPMetoyer <metoyerjeanpaul@gmail.com>
2024-01-27 14:34:52 -0600

    grill

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,16 +12,20 @@ export default function Home() {
       {/* <Offer/> */}
 
       <section id="about" className='bg-fuchsia-50'>
-        <div>
+        <div className='graphic' style={{ position: "relative" }}>
         <Image
-          src={ "/images/bacon.png" }
+          src={ "/images/grill.png" }
           alt=""
           fill
           className="object-cover"
         />
         </div>
-        <div>
+        <div className='content'>
+        <h1 className="text-3xl text-center uppercase p-2 md:p-7 md:text-4xl xl:text-4xl text-red-500">
+         Great Grillz & Glory at Game Day Grill
+        </h1>
 
+        <p>Welcome to Game Day Grill, where every meal is a touchdown of taste! Experience the ultimate blend of gourmet burgers, crafted with the freshest ingredients, in a vibrant, sports-themed ambiance. At Game Day Grill, we're not just serving food; we're creating an unforgettable dining adventure, perfect for both the casual diner and the sports enthusiast alike. Join us and savor the victory of flavor on your plate!</p>
         </div>
       </section>
 
